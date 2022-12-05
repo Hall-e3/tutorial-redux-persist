8e729463f02cf4a4fd88b56eefd5a57cd1d0d8ce
Hall Enoch Asanda <hallenochasanda@gmail.com>
2022-12-05 21:18:21 +0300

    booklist screen setup

---
+++ screens/BooksList.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function BooksList() {
+  return (
+    <View>
+      <Text>BooksList</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
